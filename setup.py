dfdb55c258697b0ab7d37ef7870f7c8d8b3828d7
mohamedaliabid10 <mohamedaliabid10@gmail.com>
2023-11-08 19:00:34 +0100

    changes

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
+
 __version__ = "0.0.0"
 
 REPO_NAME = "CNN-Classifier"
 AUTHOR_USER_NAME = "mohamedaliabid10"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "mohamedaliabid10@gmail.com"
+
 
 setuptools.setup(
     name=SRC_REPO,
