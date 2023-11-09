4e8cb549a586a53094f2e40b9265d21239237173
mohamedaliabid10 <mohamedaliabid10@gmail.com>
2023-11-09 16:25:08 +0100

    T

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cnnClassifier.pipeline.stage_01_dataingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
 from cnnClassifier import logger
 
 STAGE_NAME_1 = "Data Ingestion phase"
@@ -21,6 +22,18 @@ try:
     prepare_base_model = PrepareBaseModelTrainingPipeline()
     prepare_base_model.main()
     logger.info(f">>>>>> stage {STAGE_NAME_2} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME_3 = "Training"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME_3} started <<<<<<")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>>>>> stage {STAGE_NAME_3} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -22,3 +22,24 @@ class PrepareBaseModelConfig:
     params_include_top: bool
     params_weights: str
     params_classes: int
+
+
+# Prepare Callbacks entity
+@dataclass(frozen=True)
+class PrepareCallbacksConfig:
+    root_dir: Path
+    tensorboard_root_log_dir: Path
+    checkpoint_model_filepath: str
+
+
+# Prepare Training entity
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
